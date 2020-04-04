b40317c37d9233a85d471fc3c624162439a83a20
Grzegorz Sobociński <grzegorz.sobocinski2397@gmail.com>
2020-04-04 22:56:57 +0200

    Set correct Tab page

--- AirMonitor/AirMonitor/App.xaml.cs
+++ AirMonitor/AirMonitor/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AirMonitor.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace AirMonitor
         {
             InitializeComponent();
 
-            MainPage = new TabbedPage();
+            MainPage = new TabPage();
         }
 
         protected override void OnStart()
--- AirMonitor/AirMonitor/Views/TabbedPage.xaml.cs
+++ AirMonitor/AirMonitor/Views/TabbedPage.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace AirMonitor.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TabbedPage : ContentPage
+	public partial class TabPage : TabbedPage
 	{
-		public TabbedPage()
+		public TabPage()
 		{
 			InitializeComponent();
 		}
