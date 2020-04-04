5e6da0b80bfc51c0de5640f3ede09d5c28a17a43
Grzegorz Sobociński <grzegorz.sobocinski2397@gmail.com>
2020-04-04 22:52:49 +0200

    Create Home Page

---
+++ AirMonitor/AirMonitor/Views/HomePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AirMonitor.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+		public HomePage()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Move the user to the Home Page.
+		/// </summary>
+		private void NavigateToHomePage(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new HomePage());
+		}
+	}
+}
