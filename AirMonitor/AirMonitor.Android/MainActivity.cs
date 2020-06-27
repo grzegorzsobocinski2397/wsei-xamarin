3a0a1f4095e03c3c191f580a6bfe11328bc3bed4
Grzegorz Sobocinski <grzegorz.sobocinski2397@gmail.com>
2020-06-27 23:03:23 +0200

    Add Location permissions for >6.0 Android verisons

--- AirMonitor/AirMonitor.Android/MainActivity.cs
+++ AirMonitor/AirMonitor.Android/MainActivity.cs
@@ -1,34 +1,71 @@
-﻿using System;
-
+﻿using Android;
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Runtime;
 
 namespace AirMonitor.Droid
 {
-    [Activity(Label = "AirMonitor", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+	[Activity(Label = "AirMonitor", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		private const int RequestLocationId = 0;
 
-            base.OnCreate(savedInstanceState);
+		private readonly string[] locationPermissions =
+		{
+			Manifest.Permission.AccessCoarseLocation,
+			Manifest.Permission.AccessFineLocation
+		};
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            Xamarin.FormsMaps.Init(this, savedInstanceState);
-            LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
+			base.OnCreate(savedInstanceState);
+
+			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			Xamarin.FormsMaps.Init(this, savedInstanceState);
+			LoadApplication(new App());
+		}
+
+		protected override void OnStart()
+		{
+			base.OnStart();
+
+			if ((int)Build.VERSION.SdkInt >= 23)
+			{
+				if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+				{
+					RequestPermissions(locationPermissions, RequestLocationId);
+				}
+				else
+				{
+					System.Diagnostics.Debug.WriteLine("Localization already permission granted.");
+				}
+			}
+		}
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+		{
+			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+			if (requestCode == RequestLocationId)
+			{
+				if ((grantResults.Length == 1) && (grantResults[0] == (int)Permission.Granted))
+				{
+					System.Diagnostics.Debug.WriteLine("Localization permission granted.");
+				}
+				else
+				{
+					System.Diagnostics.Debug.WriteLine("Localization permission not granted.");
+				}
+			}
+			else
+			{
+				base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+			}
+		}
+	}
 }
